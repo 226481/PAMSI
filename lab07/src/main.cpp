184be6ca95494d7d7ea35c286b2fc2d7d1c7233f
226481 <tomasz.perczynski96@gmail.com>
2017-05-24 23:23:54 +0200

    lab07- działający main

--- lab07/src/main.cpp
+++ lab07/src/main.cpp
@@ -8,9 +8,22 @@ using namespace std;
 
 int main()
 {
-	Tree* tree=NULL;
-	Inorder(tree);
-	Preorder(tree);
-	Postorder(tree);
+	stoper s;
+	Temp* temp = new Temp;
+	
+	for(int i=0; i<1000; i++)
+	{
+	temp->Insert(rand()%100);
+	}
+		
+	temp->Inorder(temp->GetKorzen());
+	cout<<endl;
+	s.mierz_czas_start();
+	cout<<"1 - istnieje, 0 - nie istnieje: "<<temp->Find(0)<<endl;
+	s.mierz_czas_stop();
+//	cout<<endl;
+//	temp->Preorder(temp->GetKorzen());
+//	cout<<endl;
+//	temp->Postorder(temp->GetKorzen());
 	return 0;
 }
