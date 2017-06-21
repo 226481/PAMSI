ebc3dd8ffd1d981cccef6b2a68d7a4fd38367df1
226481 <tomasz.perczynski96@gmail.com>
2017-06-21 23:21:16 +0200

    plik main
    MiniMax nie dziala poprawnie, ciagle dazy do wyranej w linii prostej, od lewej do prawej

---
+++ gra/main.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<time.h>
+#include "inc/Gra.h"
+#include "src/Gra.cpp"
+
+using namespace std;
+
+//Tomasz Perczyñski, PAMSI czw. 14:00-16:00
+//Kó³ko i krzy¿yk
+
+int main() {
+	srand(time(NULL));
+	Game gra;
+	gra.Start();
+	return 0;
+}
